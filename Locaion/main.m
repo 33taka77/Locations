89869e42113eb69f3249514364c9e157cf9895f4
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-02-26 19:46:15 +0900

    Initial Commit

---
+++ Locaion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Locaion
+//
+//  Created by 相澤 隆志 on 2014/02/26.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
